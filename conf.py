12cc5128d07a285f0e61287594710398ef50c8a7
Bhaal22 <muller.john@gmail.com>
2014-10-13 04:25:17 +0000

    fixed site name

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Jonathan Muller"  # (translatable)
 BLOG_TITLE = "Scrum Programming"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link
-SITE_URL = "http://www.drylm.org:8080/"
+SITE_URL = "http://blog.drylm.org/"
 # This is the URL where nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "http://getnikola.com/"
